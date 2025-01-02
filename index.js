eea91a83eab79c4904247c59d863b2f4e0a6864f
Vinay Kumar Kharwar <95994085+onebitjoy@users.noreply.github.com>
2025-01-02 16:33:02 +0530

    adding cors

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
 
-app.use(express.static("public"))
+// Enable CORS for all routes and domains
+app.use(cors({
+    origin: '*', // Allow all origins or specify specific origins like ['https://yourdomain.com']
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow the methods you need
+}));
 
+app.use(express.static("public"))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
